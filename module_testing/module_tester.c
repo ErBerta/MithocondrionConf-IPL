d307ebb9a7da4b4ce2df503c65214763ada7d7b0
Elia Bertapelle <elia@bertapelle.it>
2020-05-06 21:40:31 +0200

    Implementati ip_mat_sub;ip_mat_sum;ip_mat_mean;

--- module_testing/module_tester.c
+++ module_testing/module_tester.c
@@ -5,9 +5,11 @@
 
 int main(){
 	Bitmap* b = NULL;
+	Bitmap* c = NULL;
 	ip_mat* input_img = NULL;
+	ip_mat* input_img2 = NULL;
 	ip_mat* mod_ip_mat = NULL;
-    b = bm_load("mongolfiere.bmp");
+  b = bm_load("mongolfiere.bmp");
 	input_img = bitmap_to_ip_mat(b);
 	ip_mat_show(input_img);
 	mod_ip_mat = ip_mat_mul_scalar(input_img, 0.1);
@@ -16,9 +18,26 @@ int main(){
 	mod_ip_mat = NULL;
 	mod_ip_mat = ip_mat_add_scalar(input_img, 0.99999);
 	ip_mat_show(mod_ip_mat);
+	mod_ip_mat = NULL;
+	c = bm_load("mongolfiere.bmp");
+	/*Necessario esegure cast a float?*/
+	input_img2 = bitmap_to_ip_mat(c);
+	ip_mat_free(mod_ip_mat);
+	mod_ip_mat = NULL;
+	mod_ip_mat = ip_mat_sum(input_img,input_img2);
+	ip_mat_show(mod_ip_mat);
+	ip_mat_free(mod_ip_mat);
+	mod_ip_mat = NULL;
+	mod_ip_mat = ip_mat_sub(input_img,input_img2);
+	ip_mat_show(mod_ip_mat);
+	ip_mat_free(mod_ip_mat);
+	mod_ip_mat = NULL;
+	mod_ip_mat = ip_mat_mean(input_img,input_img2);
+	ip_mat_show(mod_ip_mat);
 
 	bm_free(b);
 	ip_mat_free(input_img);
+	ip_mat_free(input_img2);
 	ip_mat_free(mod_ip_mat);
 
 	return 0;
